a14da1a5961df5103a8f00d338182cee48d7f6f6
anton-shikov <shik-999@inbox.ru>
2019-06-01 17:49:18 +0300

    changing code

--- LSEA.py
+++ LSEA.py
@@ -418,6 +418,7 @@ def create_clumpverse(flag, chr_snp_dict, coord_chr_snp_dict,all_snps,cl_dir='no
     return(non_hla)
 
 def make_clumps(input_dict,file_name, plink_dir, bfile):
+    print('Calculating independant loci with PLINK')
     with open(os.getcwd()+ "/"+file_name.split('.')[0]+'_for_plink.tsv','w', newline='') as csvfile:
         my_writer = csv.writer(csvfile, delimiter='\t')
         init_row = ["SNP","Chr","Pos","P"]
@@ -425,42 +426,42 @@ def make_clumps(input_dict,file_name, plink_dir, bfile):
         for snp in input_dict:
             row = [snp] + input_dict[snp][0:2]+[input_dict[snp][4]]
             my_writer.writerow(row)
-        snp_cmd = subprocess.call('{0}/plink --bfile {1} --clump {2} --clump-field P --clump-p1 5e-08 --clump-r2 0.1 --clump-snp-field SNP --clump-kb 500 --out {3}'.format(plink_dir,plink_dir +'/'+ bfile,os.getcwd()+ "/"+file_name.split('.')[0]+'_for_plink.tsv',os.getcwd()+ "/"+file_name.split('.')[0]+'.clumped'),shell=True)
+        snp_cmd = subprocess.call('{0}/plink --bfile {1} --clump {2} --clump-field P --clump-p1 5e-08 --clump-r2 0.1 --clump-snp-field SNP --clump-kb 500 --out {3} --silent 2> {4}'.format(plink_dir,plink_dir +'/'+ bfile,os.getcwd()+ "/"+file_name.split('.')[0]+'_for_plink.tsv',os.getcwd()+ "/"+file_name.split('.')[0]+'.clumped', 'PLINK_clumping.log'),shell=True)
 
 def annotate_vcf(file_name, snpeff, genome):
-
-    snp_cmd = subprocess.call('java -Xmx4g -jar {0} -v {1} {2} > {3}'.format(snpeff+'/snpEff.jar',genome,os.getcwd() + "/"+file_name,os.getcwd()+"/"+file_name.split('.')[0]+"_annotated.vcf"),shell=True)
+    print('Annotating file with snpEff')
+    snp_cmd = subprocess.call('java -Xmx4g -jar {0} -v {1} {2} > {3} 2> {4}'.format(snpeff+'/snpEff.jar',genome,os.getcwd() + "/"+file_name,os.getcwd()+"/"+file_name.split('.')[0]+"_annotated.vcf", 'snpeff.log'),shell=True)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='LSEA')
-    parser.add_argument('-af', help='Please enter input file', metavar='File',
+    parser.add_argument('-af', help='Input file in tsv-format', metavar='File',
                         type=str, required=True)
-    parser.add_argument('-vf', help='Enter vcf file', metavar='File',
+    parser.add_argument('-vf', help='Annotated vcf-file if it is already prepared ', metavar='File',
                         type=str, required=False)
-    parser.add_argument('-pl', help='Enter plink file', metavar='File',
+    parser.add_argument('-pl', help='PlINK  result of clumping (.clumped file) if it is already prepared', metavar='File',
                         type=str, required=False)
-    parser.add_argument('-cf', help='Full Manhattan cluster file', metavar='File',
-                        type=str, required=False)
-    parser.add_argument('-fa', help='Full association file', metavar='File',
-                        type=str, required=False)
-    parser.add_argument('-va', help='Full VCF annotation file', metavar='File',
-                        type=str, required=False)
-    parser.add_argument('--precompiled', '-p',action='store_true',help='use precompiled loci')
-    parser.add_argument('-sn', help='Please enter path to SNPeff', metavar='File',
+    #parser.add_argument('-cf', help='Do not use this argument, Full Manhattan cluster file', metavar='File',
+    #                    type=str, required=False)
+    #parser.add_argument('-fa', help='Do not use this argument, Full association file', metavar='File',
+    #                    type=str, required=False)
+    #parser.add_argument('-va', help='Do not use this argument, Full VCF annotation file', metavar='File',
+    #                    type=str, required=False)
+    parser.add_argument('--precompiled', '-p',action='store_true',help='Use precompiled loci')
+    parser.add_argument('-sn', help='Path to SnpEff directory', metavar='File',
                         type=str, required=True)
-    parser.add_argument('-g', help='Please enter genome for SNPeff', metavar='Str',
+    parser.add_argument('-g', help='Flag for specifying genome for SnpEff annotation', metavar='Str',
                         type=str, required=False, default='GRCh37.75')
-    parser.add_argument('-pld', help='Please enter path to plink', metavar='Dir',
+    parser.add_argument('-pld', help='Path to PLINK directory', metavar='Dir',
                         type=str, required=True)
-    parser.add_argument('-bf', help='Please enter bfile for plink', metavar='Str',
+    parser.add_argument('-bf', help='Bfile for PLINK', metavar='Str',
                         type=str, required=False, default='1000G_20101123_v3_GIANT_chr1_23_minimacnamesifnotRS_CEU_MAF0.01')
-    parser.add_argument('-cld', help='Please enter directory with clumped files', metavar='Dir',
+    parser.add_argument('-cld', help='directory with clumped files for creating universe', metavar='Dir',
                         type=str, required=False, default='')
-    parser.add_argument('--create_universe', '-cu',action='store_true',help='create universe for SNP data')
+    parser.add_argument('--create_universe', '-cu',action='store_true',help='Create universe for GWAS data')
     parser.set_defaults(feature=True)
     args = parser.parse_args()
-    af,p,vf,pl,cf,fa,va,sn,g,pld,bf,cu,cld =  args.af,args.precompiled, args.vf, args.pl,args.cf,args.fa,args.va,args.sn,args.g,args.pld,args.bf,args.create_universe,args.cld
-
+    #af,p,vf,pl,cf,fa,va,sn,g,pld,bf,cu,cld =  args.af,args.precompiled, args.vf, args.pl,args.cf,args.fa,args.va,args.sn,args.g,args.pld,args.bf,args.create_universe,args.cld
+    af,p,vf,pl,sn,g,pld,bf,cu,cld =  args.af,args.precompiled, args.vf, args.pl,args.sn,args.g,args.pld,args.bf,args.create_universe,args.cld
     all_snps = set()   
     clumpsetbd = defaultdict(list)
     genesetdb = defaultdict(list)
@@ -509,17 +510,17 @@ if __name__ == '__main__':
             for row in my_reader:
                 clumpsetbd[row[0]].extend(row[1].split(","))    
 
-    if not cf and not fa and not va:
-        with open(af,'r', newline='') as csvfile:
-            my_reader = csv.reader(csvfile, delimiter='\t')
-            for row in my_reader:
-                all_snps.add(row[2])
-                chr_snp_dict[row[0]][row[2]]=row[1]
-                coord_chr_snp_dict[row[0]][row[1]]=row[2]
-                coord_gene_dict[row[2]].append(row[0]+"_"+row[1])
-                coord_gene_dict[row[2]].append(str(0))
-                if not vf:
-                    input_dict[row[2]]=row[0:2]+row[3:]
+#    if not cf and not fa and not va:
+#        with open(af,'r', newline='') as csvfile:
+#            my_reader = csv.reader(csvfile, delimiter='\t')
+#            for row in my_reader:
+#                all_snps.add(row[2])
+#                chr_snp_dict[row[0]][row[2]]=row[1]
+#                coord_chr_snp_dict[row[0]][row[1]]=row[2]
+#                coord_gene_dict[row[2]].append(row[0]+"_"+row[1])
+#                coord_gene_dict[row[2]].append(str(0))
+#                if not vf:
+#                    input_dict[row[2]]=row[0:2]+row[3:]
 
     if vf:
         with open(vf,'r', newline='') as csvfile:
@@ -588,6 +589,7 @@ if __name__ == '__main__':
         df1 = GSEA(clumpig_universe,get_check(os.getcwd()+"/"+af.split('.')[0]+'.clumped.clumped', 'no', SNP_names_list,clumpig_universe),clumpsetbd, genesetdb, coord_gene_dict,'yes')
         if len(df1)!=0:
             df1.to_csv(af.split('.')[0]+'.enrich.tsv',sep='\t', index=False)
+            rename_cmd =subprocess.call("var=\"gene_set\tp-value\tq-value\tenrich_description\";  sed -i \"1s/.*/$var/\" {}".format(af.split('.')[0]+'.enrich.tsv'),shell=True)
     if cu:
         if cld:
             clumpig_universe = create_clumpverse('clumped.clumped', chr_snp_dict, coord_chr_snp_dict,all_snps,cl_dir=cld)
@@ -598,4 +600,7 @@ if __name__ == '__main__':
         df1 = GSEA(clumpig_universe,get_check(pl, 'no', SNP_names_list,clumpig_universe),clumpsetbd, genesetdb, coord_gene_dict,'yes')
         if len(df1)!=0:
             df1.to_csv(pl.split('.')[0]+'.enrich.tsv',sep='\t', index=False)
+            rename_cmd =subprocess.call("var=\"gene_set\tp-value\tq-value\tenrich_description\";  sed -i \"1s/.*/$var/\" {}".format(af.split('.')[0]+'.enrich.tsv'),shell=True)
+
+#"var=\"gene_set\tp-value\tq-value\tenrich_description\";echo $var;  sed -i '1s/.*/$var/' {}"
  
